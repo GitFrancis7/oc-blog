8fae5898bf3d858166c9185b9126cf89ff29b646
Francis <asjohnfrancis@gmail.com>
2019-06-06 19:11:41 +0200

    End developpement.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Post} from './post';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oc-blog';
+
+  posts: Array<Post> = [
+    new Post('Mon premier Post', 'Le code complet et fonctionnel doit être déposé dans un dépôt Git en ligne' +
+      ' que les validateurs doivent pouvoir cloner. Vous fournirez le lien vers le dépôt dans le champs de commentaires.\'', 0),
+    new Post('Un Autre Post', 'Assuming that the CLI ng serve command is still running, ' +
+      'the browser should refresh and display both the application title and the hero name.', 1),
+    new Post('Un Super Post', 'Assuming that the CLI ng serve command is still running, ' +
+      'the browser should refresh and display both the application title and the hero name.', 3),
+    new Post('Un Mega Post', 'Assuming that the CLI ng serve command is still running, ' +
+      'the browser should refresh and display both the application title and the hero name.', -2)
+  ];
+
+
+  constructor() {
+  }
 }
